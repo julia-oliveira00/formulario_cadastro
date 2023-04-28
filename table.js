dc87106d08c16727de268c331e2bd75295e821a6
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2023-04-28 10:42:08 -0300

    finalização

--- table.js
+++ table.js
@@ -1,13 +1,24 @@
 function Validate()
 {
-    let name            = document.getElementById('name') .value;
-    let email           = document.getElementById('email') .value;
-    let fone            = document.getElementById('fone') .value;
-    let date            = document.getElementById('date') .value;
-    let =document.getElementById('') .value;
-    let =document.getElementById('') .value;
-    let =document.getElementById('') .value;
-    let =document.getElementById('') .value;
-    let password        =document.getElementById('password') .value;
-    let passwordconfirm =document.getElementById('passwordconfirm') .value;
+    let name            = document.getElementById('name').value;
+    let email           = document.getElementById('email').value;
+    let fone            = document.getElementById('fone').value;
+    let date            = document.getElementById('date').value;
+    let cpf             = document.getElementById('cpf').value;
+    let state           = document.getElementById('state').value;
+    let city            = document.getElementById('city').value;
+    let neighbourhood   = document.getElementById('neighbourhood').value;
+    let street          = document.getElementById('street').value;
+    let number          = document.getElementById('number').value;
+    let more            = document.getElementById('more').value;
+    let password        = document.getElementById('password').value;
+    let passwordconfirm = document.getElementById('passwordconfirm').value;
+
+    if (!name || !email || !fone || !date || !cpf || !state || !city || !neighbourhood || !street || !number || !password || !passwordconfirm)
+    {
+        alert("Preencha todos os campos obrigatórios!");
+    } else 
+    {
+        alert("Cadastro efetuado com sucesso!")
+    }
 }
