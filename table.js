35c97c0e4400cd8cbb1b91ea755355b4145dc17f
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2023-04-28 08:05:46 -0300

    meio do projeto

---
+++ table.js
@@ -0,0 +1,13 @@
+function Validate()
+{
+    let name            = document.getElementById('name') .value;
+    let email           = document.getElementById('email') .value;
+    let fone            = document.getElementById('fone') .value;
+    let date            = document.getElementById('date') .value;
+    let =document.getElementById('') .value;
+    let =document.getElementById('') .value;
+    let =document.getElementById('') .value;
+    let =document.getElementById('') .value;
+    let password        =document.getElementById('password') .value;
+    let passwordconfirm =document.getElementById('passwordconfirm') .value;
+}
